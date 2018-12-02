1f00706a781ae0411395221d1f6873bf745d753f
sdfg <werfsfdsf@iosdufoi.com>
2018-12-02 20:53:29 +0530

    Intial checkin

---
+++ src/routes/Todos/routes/Manage/components/TodoList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Todo } from './Todo';
+import { Col, Row } from 'react-flexbox-grid/lib';
+
+export function TodoList(props) {
+  const { todos, toggle } = props;
+
+  const renderTodos = todos.map((todo) => {
+    const { name, completed, id } = todo;
+    return (
+      <Todo
+        name={name}
+        toggle={toggle}
+        key={id}
+        id={id}
+        completed={completed}
+      />
+    );
+  });
+
+  return (
+    <div>
+      {renderTodos}
+    </div>
+  );
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.object),
+  toggle: PropTypes.func,
+};
